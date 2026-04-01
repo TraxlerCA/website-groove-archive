9b7bdce248a504d26e24645eb6b33676d905ebaf
TraxlerCA <joostnulnegen@gmail.com>
2026-04-01 21:57:18 +0200

    Force dynamic homepage shuffle seed

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -2,6 +2,8 @@ import { randomUUID } from 'node:crypto';
 
 import RecordBinHome from '@/components/home/RecordBinHome';
 
+export const dynamic = 'force-dynamic';
+
 export default function HomePage() {
   return <RecordBinHome shuffleSeed={randomUUID()} />;
 }
